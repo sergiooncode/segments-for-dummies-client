92334e9ad468836c74d80599c2e9c6dda22bc69f
Sergio Perez <SPerez@leapfrogonline.com>
2016-01-08 17:09:23 -0600

    hardcoded shoppers until i can make it work

--- test_app/assets/scripts/app_controller.js
+++ test_app/assets/scripts/app_controller.js
@@ -2,13 +2,19 @@ angular.module('app').
     controller('AppController',
               ['$scope', '$http',
               function($scope, $http) {
-    return $http.jsonp('http://localhost:4000/api/shoppers', {
-    }).then(
-     response => {
-         return response.data
-     },
-     response => {
-         return response
-     }
-    );
+
+    /**$scope.shoppers = function() {
+        return $http.jsonp('http://localhost:4000/api/shoppers', {
+        }).then(
+          response => {
+              return response.data;
+          },
+          response => {
+              return response;
+          }
+        );
+    };**/
+
+    $scope.shoppers = ['one shopper', 'another shopper'];
+
 }]);
