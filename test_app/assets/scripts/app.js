6075c5a9649fc813c4c1caf51990c52bd6da3987
Sergio Perez <SPerez@leapfrogonline.com>
2016-01-08 17:07:13 -0600

    trick to be able to use both angular and jinja curly braces

--- test_app/assets/scripts/app.js
+++ test_app/assets/scripts/app.js
@@ -1,5 +1,9 @@
-angular.module('app', [
-]);
+angular.module('app', []);
+
+angular.module('app').config(['$interpolateProvider', function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[');
+    $interpolateProvider.endSymbol(']}');
+}]);
 
 /**angular.module('app').config(['$urlRouterProvider'], function($urlRouterProvider) {
     $urlRouterProvider.otherwise("/");
